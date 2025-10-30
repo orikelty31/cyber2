bfaa91b54e66b56b604128cd49bd9cc648eaa9b7
orike <orikelty31@gmail.com>
2025-10-30 16:20:53 +0200

    Update

--- client.py
+++ client.py
@@ -1,26 +1,29 @@
 import socket
 
 MAX_PACKET = 1024
-
-option = None
-while(option != "EXIT"):
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        my_socket.connect(('127.0.0.1', 1729))
-        my_socket.send('Hello There'.encode())
-    except socket.error as err:
-        print('received socket error ' + str(err))
-    finally:
-        print("Hello Welcome To Our Server Please Choose One Of The Four Options You Can Choose")
-        print("You Have TIME / NAME / RAND / EXIT")
-        option = input("Please Choose One Of The Options: ").upper()
-        if(option):
-            my_socket.send(option.encode())
-        else:
-            while(not option):
-                print("Please Write Something")
-                option = input("Please Choose One Of The Options: ").upper()
+def main():
+    option = None
+    while(option != "EXIT"):
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            my_socket.connect(('127.0.0.1', 1729))
+            my_socket.send('Hello There'.encode())
+        except socket.error as err:
+            print('received socket error ' + str(err))
+        finally:
+            print("Hello Welcome To Our Server Please Choose One Of The Four Options You Can Choose")
+            print("You Have TIME / NAME / RAND / EXIT")
+            option = input("Please Choose One Of The Options: ").upper()
+            if(option):
                 my_socket.send(option.encode())
-        response = my_socket.recv(MAX_PACKET).decode()
-        print(response)
-        my_socket.close()
+            else:
+                while(not option):
+                    print("Please Write Something")
+                    option = input("Please Choose One Of The Options: ").upper()
+                    my_socket.send(option.encode())
+            response = my_socket.recv(MAX_PACKET).decode()
+            print(response)
+            my_socket.close()
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -7,30 +7,34 @@ time = datetime.datetime.now()
 SERVER_NAME = "Ori Server"
 QUEUE_LEN = 1
 
-
-while True:
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        my_socket.bind(('0.0.0.0', 1729))
-        my_socket.listen(QUEUE_LEN)
-        client_socket, client_address = my_socket.accept()
-        try :
-            request = client_socket.recv(MAX_PACKET).decode()
-            print('server received ' + request)
+def main():
+    while True:
+        my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            my_socket.bind(('0.0.0.0', 1729))
+            my_socket.listen(QUEUE_LEN)
+            client_socket, client_address = my_socket.accept()
+            try :
+                request = client_socket.recv(MAX_PACKET).decode()
+                print('server received ' + request)
+            except socket.error as err:
+                print('received socket error on client socket' + str(err))
+            finally:
+                mode = client_socket.recv(MAX_PACKET).decode()
+                if(mode == "TIME"):
+                    client_socket.send(f"Current date and time: {time}".encode())
+                elif(mode == "NAME"):
+                    client_socket.send(('The Server Name Is: ' + SERVER_NAME).encode())
+                elif(mode == "RAND"):
+                    client_socket.send(('The Random Number Is: ' + str(random.randint(1,10))).encode())
+                else:
+                    print("The Client Didn't Want Nothing")
+                client_socket.close()
         except socket.error as err:
-            print('received socket error on client socket' + str(err))
+            print('received socket error on server socket' + str(err))
         finally:
-            mode = client_socket.recv(MAX_PACKET).decode()
-            if(mode == "TIME"):
-                client_socket.send(f"Current date and time: {time}".encode())
-            elif(mode == "NAME"):
-                client_socket.send(('The Server Name Is: ' + SERVER_NAME).encode())
-            elif(mode == "RAND"):
-                client_socket.send(('The Random Number Is: ' + str(random.randint(1,10))).encode())
-            else:
-                print("The Client Didn't Want Nothing")
-            client_socket.close()
-    except socket.error as err:
-        print('received socket error on server socket' + str(err))
-    finally:
-        my_socket.close()
+            my_socket.close()
+
+
+if __name__ == '__main__':
+    main()
